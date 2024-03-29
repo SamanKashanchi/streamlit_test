2d2c2b3e177142c6765ce731e5a8918b3925bac8
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-03-28 19:32:15 -0700

    Add files via upload

--- streamlit_cloud.py
+++ streamlit_cloud.py
@@ -2,6 +2,7 @@ import streamlit as st
 from moviepy.editor import VideoFileClip
 import os
 import tempfile
+import base64
 
 st.write('Hello world! How are yous. Its time to tawek over the world')
 
