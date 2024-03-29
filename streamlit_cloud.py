94a7c21bbc81f164ae4b101d93749c4d1e2cbb43
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-03-28 21:39:53 -0700

    Add files via upload

--- streamlit_cloud.py
+++ streamlit_cloud.py
@@ -33,9 +33,11 @@ def split_video_into_chunks(video_path, chunk_duration):
         chunks.append(chunk)
     return chunks
 
+st.markdown("---")
+
+
 st.subheader("Video Splitter")
 
-st.markdown("---")
 
 chunk_size = st.text_input("How many seconds do you want each chunk to be: ")
 uploaded_file = st.file_uploader("Upload a video and split into customizbale chunks: ", type=["mp4", "avi", "mov"])
