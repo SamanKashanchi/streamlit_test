927437fd2e915f73844562ffb9cccaf312d670de
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-03-28 18:46:51 -0700

    Add files via upload

--- streamlit_cloud.py
+++ streamlit_cloud.py
@@ -8,4 +8,4 @@ st.write('Hello world! How are yous. Its time to tawek over the world')
 uploaded_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
 
 if uploaded_file is not None:
-st.video(uploaded_file)
+    st.video(uploaded_file)
