67c88d397fb489e1f5977262256509a3e535d7ff
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-03-28 19:28:20 -0700

    Add files via upload

--- streamlit_cloud.py
+++ streamlit_cloud.py
@@ -9,22 +9,25 @@ uploaded_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
 
 if uploaded_file is not None:
     st.video(uploaded_file)
+    
 
-
+    file_content = uploaded_file.getvalue()
     file_name = uploaded_file.name
-    with open(file_name,"wb") as f:
-        f.write(uploaded_file.getvalue())
-
-    st.success(f"File {file_name} has been uploaded successfully")
+      
+   
 
 
-    # Get user's desktop directory
-    desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop")
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+        temp_file.write(file_content)
 
-    # Move uploaded file to desktop
-    desktop_file_path = os.path.join(desktop_dir, file_name)
 
-    st.text(desktop_file_path)
+    # # Get user's desktop directory
+    # desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop")
+
+    # # Move uploaded file to desktop
+    # desktop_file_path = os.path.join(desktop_dir, file_name)
+
+    # st.text(desktop_file_path)
 
     # os.rename(file_name, desktop_file_path)
 
