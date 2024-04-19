cfe9c69440c251ba621588ccf353e387b418f320
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-19 16:54:03 -0700

    Add files via upload

--- streamlit_cloud.py
+++ streamlit_cloud.py
@@ -26,7 +26,7 @@ sell_signals = []
 short_signals = []
 cover_signals = []
 balance_over_time = []  # To store balance over time
-balance_over_time.append(data['Date'][0], balance)
+balance_over_time.append((data['Date'][0], balance))
 
 # Loop through the data to identify buy, sell, short, and cover signals
 for date, close_price, mean, buy_thresh, short_thresh in zip(data['Date'], data['Close'], rolling_mean, buy_threshold, short_threshold):
