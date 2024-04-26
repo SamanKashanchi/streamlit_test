11bf7285712800ff3bba81d5e5105302eecc1fd0
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-26 16:22:00 -0700

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -1,15 +1,5 @@
 import streamlit as st
-import  yfinance as yf
-import pandas as pd
-import plotly.graph_objs as go
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings
-
-from scipy.stats import shapiro
-import statsmodels.api as sm
-import numpy as np
 
 st.set_page_config(page_title="PORTFOLIO")
 
