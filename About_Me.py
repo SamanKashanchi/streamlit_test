2b70e34dafb5d1e2971219bb03ab23791c2d4b5d
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-26 23:00:06 -0700

    Add files via upload

--- About_Me.py
+++ About_Me.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import  yfinance as yf
+import pandas as pd
+import plotly.graph_objs as go
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+from scipy.stats import shapiro
+import statsmodels.api as sm
+import numpy as np
 
 st.set_page_config(page_title="PORTFOLIO")
 
+
+# Load your profile picture
+profile_pic = 'profile.jpeg'  
+
+st.sidebar.image(profile_pic, caption='Your Name', use_column_width=True)
