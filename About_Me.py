32587c5e9733a8c22aa37dde505731ad2c6b619c
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-27 00:39:34 -0700

    Add files via upload

--- About_Me.py
+++ About_Me.py
@@ -20,10 +20,11 @@ profile_pic = 'profile.jpeg'
 
 st.sidebar.image(profile_pic, caption='Saman Kashanchi', use_column_width=True)
 
-def gradient(color1, color2, color3, content1):
+def gradient(color1, color2, color3, content1, content2):
     # Create an HTML structure with styling for a gradient header
     st.markdown(f'<h1 style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});font-size:60px;border-radius:2%;">'
-                f'<span style="color:{color3};">{content1}</span><br>', 
+                f'<span style="color:{color3};">{content1}</span><br>'
+                f'<span style="color:white;font-size:17px;">{content2}</span></h1>',, 
                 unsafe_allow_html=True)
 
 # Load a Lottie animation from a URL
@@ -37,23 +38,15 @@ python_lottie = load_lottieurl("https://lottie.host/9127127e-e4e5-494b-85a2-0aa3
 
 sql_lottie = load_lottieurl('https://lottie.host/365ff170-3177-4554-be41-065a379a505e/gMAp1Y5Hk9.json')
 
+content2 = "From data to decisions: Unleashing the power of data science and predictive analytics to empower businesses"
 
 # Call the "gradient" function to display a gradient title
-gradient('#FFD4DD','#000395','e0fbfc',f"Hi, I'm Saman👋")
+gradient('#FFD4DD','#000395','e0fbfc',f"Hi, I'm Saman👋", content2)
 st.write("")  # Add an empty line
 
 
 
-st.markdown("""
-    <style>
-        .wide-text {
-            width: 100%;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-st.markdown('<p class="wide-text">Versatile and accomplished Data Scientist with a solid foundation in Computational Data Science and Mathematics, boasting a record of initiating and leading data-driven projects to successful completion. Proficient in leveraging advanced analytics, machine learning models, and data visualization techniques to drive strategic business decisions and operational efficiencies. Recognized for excellence in automation, demand forecasting, and research contributions.</p>', unsafe_allow_html=True)
-
+st.write('Versatile and accomplished Data Scientist with a solid foundation in Computational Data Science and Mathematics, boasting a record of initiating and leading data-driven projects to successful completion. Proficient in leveraging advanced analytics, machine learning models, and data visualization techniques to drive strategic business decisions and operational efficiencies. Recognized for excellence in automation, demand forecasting, and research contributions')
 st.subheader('Skills')
 st_lottie(python_lottie, height=90,width=90, key="python", speed=2.5)
 st_lottie(sql_lottie, height=90,width=90, key="sql", speed=2.5)
