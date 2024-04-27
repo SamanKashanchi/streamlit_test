5ce626db300883d03997b100b3433900532857f4
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-26 23:02:14 -0700

    Add files via upload

--- About_Me.py
+++ About_Me.py
@@ -10,10 +10,11 @@ from scipy.stats import shapiro
 import statsmodels.api as sm
 import numpy as np
 
-st.set_page_config(page_title="PORTFOLIO")
 
 
 # Load your profile picture
 profile_pic = 'profile.jpeg'  
 
 st.sidebar.image(profile_pic, caption='Your Name', use_column_width=True)
+
+st.set_page_config(page_title="PORTFOLIO")
