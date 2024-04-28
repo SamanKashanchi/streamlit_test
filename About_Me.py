9abbf0fae5e529dd1731f5e252f6fd7037f3eeab
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-27 19:16:18 -0700

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -53,6 +53,6 @@ st.subheader('Skills')
 st_lottie(python_lottie, height=90,width=90, key="python", speed=2.5)
 st_lottie(sql_lottie, height=90,width=90, key="sql", speed=2.5)
 st_lottie(docker_lottie, height=90,width=90, key="docker", speed=2.5)
-st_lottie("mongodb.json", height=90,width=90, key="sql", speed=2.5)
+st_lottie("mongodb.json", height=90,width=90, key="mangodb", speed=2.5)
 
 
