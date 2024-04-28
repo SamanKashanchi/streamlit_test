e1a57b0bc6160f832b004e842ea7828cb8446ecd
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-27 19:13:19 -0700

    Update 3_Research.py

--- pages/3_Research.py
+++ pages/3_Research.py
@@ -1,6 +1,13 @@
 
 import streamlit as st
 
+# Load your profile picture
+profile_pic = 'profile_pic.jpeg'  
+
+# st.sidebar.image(profile_pic, caption='Your Name', use_column_width=True)
+st.sidebar.image(profile_pic, caption='Saman Kashanchi', use_column_width=True)
+
+
 st.subheader('Research')
 
 reserch_pic = 'research.jpg'  
