b3077d8fd37dfe5209c6869a202bd5c51116fbbd
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-04-28 23:46:11 -0700

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -64,6 +64,6 @@ with st.container():
         st_lottie(docker_lottie, height=90,width=90, key="docker", speed=2.5)
 
     with col3:
-        st.markdown('<img width="48" height="48" src="https://img.icons8.com/color/48/amazon-web-services.png" alt="amazon-web-services"/>', unsafe_allow_html=True)
-        st.markdown('<img width="48" height="48" src="https://img.icons8.com/color/48/power-bi.png" alt="power-bi"/>', unsafe_allow_html=True)
+        st.markdown('<img width="90" height="90" src="https://img.icons8.com/color/48/amazon-web-services.png" alt="amazon-web-services"/>', unsafe_allow_html=True)
+        st.markdown('<img width="90" height="90" src="https://img.icons8.com/color/48/power-bi.png" alt="power-bi"/>', unsafe_allow_html=True)
 
