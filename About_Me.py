8d6b648e77cb924ada5f34816148acda7274fd44
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-08 20:32:38 -0700

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -34,42 +34,6 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-st.markdown(
-    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">',
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    """
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
-        <a class="navbar-brand" href="#">Kim Marcial A. Vallesteros</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#skills"
-                    >Skills Summary</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#projects">Projects</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#contact">Contact Me</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    """,
-    unsafe_allow_html=True
-)
-
-
 python_lottie = load_lottieurl("https://lottie.host/9127127e-e4e5-494b-85a2-0aa303879534/KNejozHiWc.json")
 
 sql_lottie = load_lottieurl('https://lottie.host/365ff170-3177-4554-be41-065a379a505e/gMAp1Y5Hk9.json')
