1eeb46da6bff286edcfd7e2e2fd084538700c507
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-10 14:30:17 -0700

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -161,6 +161,8 @@ with st.container():
     with col2:
         st_lottie(docker_lottie, height=90,width=90, key="docker", speed=2.5)
         st.markdown('<img width="60" height="60" src="https://icon.icepanel.io/Technology/svg/Matplotlib.svg" alt="power-bi"/>', unsafe_allow_html=True)
+        st.markdown("<p style='margin: 5px 0;'>matplotlib</p>")
+
 
     with col3:
         st.markdown('<img width="60" height="60" src="https://img.icons8.com/color/48/amazon-web-services.png" alt="amazon-web-services"/>', unsafe_allow_html=True)
