9e5cdf29d995219b88f6f389d746be7546b23fcf
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-13 16:58:07 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -111,7 +111,7 @@ with st.container():
                 "</div>"
                 "<p style='margin: 10px 0;'>Python</p>"
                 f"<div style='display: flex; justify-content: center;'>"
-                f"<img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg' alt='c' style='width: 50px; height: 50px;
+                f"<img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg' alt='c' style='width: 50px; height: 50px;'>"
                 "</div>"
                 "<p style='margin: 10px 0;'>C</p>"
                 "</div>"
