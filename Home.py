8ad07d26189d86e560c036282444660d18dc0f92
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-14 15:26:49 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -76,11 +76,11 @@ with st.container():
         data= PDFbyte,
         file_name=resume_file,
         mime="application/octet-stream")
-
-
-    with middle_column:
         st.markdown(
         f'<a href="{linkedin_url}" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="30" style="vertical-align: middle;"></a>', unsafe_allow_html=True)
+
+    with middle_column:
+       
         # st.empty()
     with right_column:
         
