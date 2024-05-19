3dfca870c33f2f291a4bf3b367d94be48a0713cf
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-18 17:19:59 -0700

    Create 4_Custom_agent.py

---
+++ pages/4_Custom_agent.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+from llama_index.query_engine import PandasQueryEngine
+# from prompts import context
+from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.agent import ReActAgent
+from llama_index.llms import OpenAI
+# from pdf import saman_engine
