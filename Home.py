75128f21eb7dd5b75bf94a22aded9f8b818f079a
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-19 16:10:57 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,6 +12,8 @@ import numpy as np
 import requests
 from streamlit_lottie import st_lottie
 
+from llama_index.llms import OpenAI
+
 
 st.set_page_config(page_title="PORTFOLIO", layout ='wide')
 st.markdown(
