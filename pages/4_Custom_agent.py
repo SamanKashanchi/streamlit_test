76d026ca410c601583b3f9ab1a316601d55f9966
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-18 17:44:05 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from llama_index import * 
 
+llm = OpenAI(model = "gpt-3.5-turbo-0613")
 
 agent = ReActAgent.from_tools(tools, llm = llm, verbose = True, context = "SAY HELLO")
 
