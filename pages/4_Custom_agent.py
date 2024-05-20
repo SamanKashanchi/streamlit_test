c994b415935ff070ceac6fbbdb5919346a82f355
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-20 15:55:03 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -4,7 +4,7 @@ import streamlit as st
 import llama_index
 from llama_index.llms.openai import OpenAI
 from llama_index.core.agent import ReActAgent
-from llama_index.core.tools import QueryEngineTool
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
 
 
 import subprocess
