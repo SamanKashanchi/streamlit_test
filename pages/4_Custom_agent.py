621632b4465933bff0509ddf2415c59cd7a53b07
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-21 13:39:25 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -12,7 +12,7 @@ from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_s
 
 
 # Ask user to enter OpenAI API key
-openai_api_key = st.text_input("Enter your OpenAI API Key", type='password',help="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key")
+openai_api_key = st.text_input("Enter your OpenAI API Key", type='password',help="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key", key='API_KEY')
 
 # Create a button for the user to submit their API key
 if st.button('Submit'):
