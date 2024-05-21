fe0bd14a9305d1c41ae4f4b9f81b71501cabde15
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-20 17:08:54 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -8,7 +8,20 @@ from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.readers.file import PDFReader
 from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
 
+def get_index(data, index_name):
+    index = None
+
+    if not os.path.exists(index_name):
+        print("Building index", index_name)
+        index = VectorStoreIndex.from_documents(data, show_progress = True)
+        index.storage_context.persist(persist_dir = index_name)
+    else:
+        index = load_index_from_storage(
+            StorageContext.from_defaults(persist_dir = index_name)
+        )
+    return index
 
 
 pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
 saman_pdf = PDFReader().load_data(file = pdf_path)
+saman_index = get_index(saman_pdf, 'saman')
