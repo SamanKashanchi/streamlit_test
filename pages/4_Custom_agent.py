a10e5e5dfe2ccdb8d7ec98195d842f5ec46c89dd
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-21 13:30:26 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -40,7 +40,7 @@ def get_index(data, index_name):
 # openai_api_key = os.getenv('OPENAI_API_KEY')
 # openai.api_key = openai_api_key
 
-openai.api_key = st.secrets['OPENAI_API_KEY']
+# openai.api_key = st.secrets['OPENAI_API_KEY']
 
 if not openai_api_key:
     st.text("No OPENAI_API_KEY found in environment variables")
