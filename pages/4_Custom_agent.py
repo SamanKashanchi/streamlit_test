75969d0d11ac683d15228b710777ca67272ca8eb
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-21 13:29:49 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -9,30 +9,6 @@ from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.readers.file import PDFReader
 from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
 
-# def get_index(data, index_name):
-#     index = None
-
-#     if not os.path.exists(index_name):
-#         print("Building index", index_name)
-#         index = VectorStoreIndex.from_documents(data, show_progress = True)
-#         index.storage_context.persist(persist_dir = index_name)
-#     else:
-#         index = load_index_from_storage(
-#             StorageContext.from_defaults(persist_dir = index_name)
-#         )
-#     return index
-
-# # openai_api_key = os.getenv('OPENAI_API_KEY')
-# # openai.api_key = openai_api_key
-
-# openai.api_key = st.secrets['OPENAI_API_KEY']
-
-# if not openai_api_key:
-#     st.text("No OPENAI_API_KEY found in environment variables")
-
-# pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
-# saman_pdf = PDFReader().load_data(file = pdf_path)
-# saman_index = get_index(saman_pdf, 'saman')
 
 
 # Ask user to enter OpenAI API key
@@ -46,9 +22,30 @@ if st.button('Submit'):
     openai.api_key = openai_api_key
     
     # Check if the API key is valid by making a simple API call
-    try:
-        openai.Engine.list()
 
-        st.success("API key is valid!")
-    except Exception as e:
-        st.error("Error testing API key: {}".format(e))
+
+def get_index(data, index_name):
+    index = None
+
+    if not os.path.exists(index_name):
+        print("Building index", index_name)
+        index = VectorStoreIndex.from_documents(data, show_progress = True)
+        index.storage_context.persist(persist_dir = index_name)
+    else:
+        index = load_index_from_storage(
+            StorageContext.from_defaults(persist_dir = index_name)
+        )
+    return index
+
+# openai_api_key = os.getenv('OPENAI_API_KEY')
+# openai.api_key = openai_api_key
+
+openai.api_key = st.secrets['OPENAI_API_KEY']
+
+if not openai_api_key:
+    st.text("No OPENAI_API_KEY found in environment variables")
+
+pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
+saman_pdf = PDFReader().load_data(file = pdf_path)
+saman_index = get_index(saman_pdf, 'saman')
+
