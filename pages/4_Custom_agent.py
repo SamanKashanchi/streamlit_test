0b2a2c7706f9c1f0dc992a10b53b2a523ad9c494
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-31 10:10:09 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -10,6 +10,23 @@ from llama_index.readers.file import PDFReader
 from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
 
 
+def gradient(color1, color2, color3, content1, content2):
+    # Create an HTML structure with styling for a gradient header
+    st.markdown(f'<h1 style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});font-size:60px;border-radius:2%;">'
+                f'<span style="color:{color3};font-family:Calibri;">{content1}</span><br>'
+                f'<span style="color:white;font-size:17px;">{content2}</span></h1>',
+                unsafe_allow_html=True)
+
+
+with st.container():
+    # Divide the container into two columns, with widths 8 and 3
+    col1, col2 = st.columns([8, 3])
+
+with col1:
+    # Call the "gradient" function to display a gradient title
+    gradient('#FF5733','#1B1464','e0fbfc',f"Research", 'Led the development of innovative Neural Network architectures tailored for the optics physics department.')   
+
+
 
 # Ask user to enter OpenAI API key
 openai_api_key = st.text_input("Enter your OpenAI API Key", type='password',help="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key", key='API_KEY')
