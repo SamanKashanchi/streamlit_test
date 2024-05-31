c80266f3649162a73ae7c23ccd91214fbc85a601
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-05-31 13:30:34 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -25,7 +25,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-chatbot_lottie = load_lottieurl("https://lottie.host/50f9b761-80d7-4db0-ba4c-f10325d58026/BCGz88DAGE.json")
+chatbot_lottie = load_lottieurl("https://lottie.host/429121f4-7fdd-44d8-aebe-1eefd412e107/XsLdWEqciy.json")
 
 with st.container():
     col1, col2 = st.columns([8, 3])
