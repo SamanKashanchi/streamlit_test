d68b7a86b53f650f929e5587848dfc471689944d
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-06-03 14:58:03 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -99,6 +99,7 @@ agent = ReActAgent.from_tools(tools, llm = llm, verbose = True, context = contex
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+st.session_state.messages = []
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -115,8 +116,8 @@ if prompt :
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    response  = st.write_stream(result)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response })
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response  = st.write_stream(result)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response })
