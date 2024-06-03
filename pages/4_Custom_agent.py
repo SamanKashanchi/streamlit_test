e4a612a3fd558c1e70ffe158602d48d44a151716
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-06-03 14:57:31 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -117,6 +117,6 @@ if prompt :
     
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
-    response = st.write_stream(result)
+    response  = st.write_stream(result)
 # Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": result})
+st.session_state.messages.append({"role": "assistant", "content": response })
