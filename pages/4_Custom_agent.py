ebfacfd37dc48f3c89368619ff09ab80866bf847
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-06-13 12:44:10 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -69,31 +69,28 @@ if 'OPENAI_API_KEY' not in os.environ:
         
         # Check if the API key is valid by making a simple API call
         os.environ["OPENAI_API_KEY"] = openai_api_key
+    
+        
+        # Check if the API key is valid by making a simple API call
+        os.environ["OPENAI_API_KEY"] = openai_api_key
+    
+        st.text(openai_api_key)
+    
+        context = """Purpose: The primary role of this agent is to assist users by providing accurate 
+                    information Saman, his experiences and his background. """
+        
+        
+        pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
+        saman_pdf = PDFReader().load_data(file = pdf_path)
+        saman_index = get_index(saman_pdf, 'saman PDF')
+        saman_engine = saman_index.as_query_engine()
 
 
 # openai_api_key = st.text_input("Enter your OpenAI API Key", type='password',help="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key", key='API_KEY')
 
 
-# submit = st.button('Submit')
-# Create a button for the user to submit their API key
-if submit:
-    # Set the OpenAI API key as an environment variable
-    # Set the OpenAI API key directly
-    openai.api_key = openai_api_key
-    
-    # Check if the API key is valid by making a simple API call
-    os.environ["OPENAI_API_KEY"] = openai_api_key
 
-    st.text(openai_api_key)
-
-    context = """Purpose: The primary role of this agent is to assist users by providing accurate 
-                information Saman, his experiences and his background. """
-    
-    
-    pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
-    saman_pdf = PDFReader().load_data(file = pdf_path)
-    saman_index = get_index(saman_pdf, 'saman PDF')
-    saman_engine = saman_index.as_query_engine()
+  
 
 
 
