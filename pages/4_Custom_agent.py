48448f0198467fe8d7e7e04b28e307c2db626a3b
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-07-05 14:47:39 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -64,21 +64,20 @@ tab1, tab2 = st.tabs([t.center(9, "\u2001") for t in tabs])
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-st.text(f"Loaded API Key: {openai_api_key}")
+
+if openai_api_key:
+    st.text("LOADED OPENAI API KEY")
+else:
+    st.text("MISSING OPENAI API KEY")
+
+context = """Purpose: The primary role of this agent is to assist users by providing accurate 
+            information Saman, his experiences and his background. """
 
 
-
-# openai_api_key = secrets['OPENAI_API_KEY']
-# st.text(f"Loaded API Key: {openai_api_key}")
-    
-# context = """Purpose: The primary role of this agent is to assist users by providing accurate 
-#             information Saman, his experiences and his background. """
-
-
-# pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
-# saman_pdf = PDFReader().load_data(file = pdf_path)
-# saman_index = get_index(saman_pdf, 'saman PDF')
-# saman_engine = saman_index.as_query_engine()
+pdf_path = os.path.join("data", "Saman Kashanchi Resume 2024.pdf")
+saman_pdf = PDFReader().load_data(file = pdf_path)
+saman_index = get_index(saman_pdf, 'saman PDF')
+saman_engine = saman_index.as_query_engine()
 
     
 
