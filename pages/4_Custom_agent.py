39feae5b598fc97acb8b4a823425422ebee8eee0
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-07-05 14:27:19 -0700

    Update 4_Custom_agent.py

--- pages/4_Custom_agent.py
+++ pages/4_Custom_agent.py
@@ -62,12 +62,14 @@ tabs = ["Llama-Index OpenAI Portolio Agent", "LangChain Llama3 Chatbot"]
 tab1, tab2 = st.tabs([t.center(9, "\u2001") for t in tabs])
 
 secrets_path = os.path.join(".streamlit", "secrets.toml")
-secrets = toml.load(secrets_path)
+st.text(secrets_path)
 
-st.text(secrets)
+# secrets = toml.load(secrets_path)
 
-openai_api_key = secrets['OPENAI_API_KEY']
-st.text(f"Loaded API Key: {openai_api_key}")
+# st.text(secrets)
+
+# openai_api_key = secrets['OPENAI_API_KEY']
+# st.text(f"Loaded API Key: {openai_api_key}")
     
 # context = """Purpose: The primary role of this agent is to assist users by providing accurate 
 #             information Saman, his experiences and his background. """
