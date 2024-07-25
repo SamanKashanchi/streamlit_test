9ea6ef85e51a93efb8d40c0e8c52210408cb6e21
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-07-25 10:45:14 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -135,7 +135,6 @@ st.write()
 
 with st.container():
     st.header('⚒️ Skills', divider='red')
-    st.markdown('<hr style="border-top: 2px solid red;">', unsafe_allow_html=True)
     col1, col2, col3= st.columns([1, 1, 1])
     with col1:
 
