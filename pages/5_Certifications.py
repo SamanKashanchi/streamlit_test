41e72f9c934cd9e2a2467b82ab929552e9a6f6b8
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-09-17 16:31:31 -0700

    Update 5_Certifications.py

--- pages/5_Certifications.py
+++ pages/5_Certifications.py
@@ -1,3 +1,4 @@
 import streamlit as st
-
+certificate_url = "https://www.testdome.com/certificates/fa5728523f994b2ea040e0d5cf8870f6"
+components.iframe(certificate_url, height=600)
 st.markdown("[View my certificate](https://www.testdome.com/certificates/fa5728523f994b2ea040e0d5cf8870f6)")
