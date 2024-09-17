a322658b57af438ae824106e97f8e6dc2bdfe884
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-09-17 16:42:24 -0700

    Update 5_Certifications.py

--- pages/5_Certifications.py
+++ pages/5_Certifications.py
@@ -2,5 +2,5 @@ import streamlit as st
 from PIL import Image
 from datetime import datetime
 
-st.image("https://github.com/SamanKashanchi/streamlit_test/blob/main/TestDomePython.PNG")
+st.image("TestDomePython.PNG")
 
