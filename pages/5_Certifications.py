62c71beced587ed8963dcd6bea920d7a565ccf57
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-09-17 16:38:26 -0700

    Update 5_Certifications.py

--- pages/5_Certifications.py
+++ pages/5_Certifications.py
@@ -10,7 +10,7 @@ def show_certifications():
         {
             "title": "TestDome Python Certificate",
             "date": "September 2024",
-            "image_url": "https://www.testdome.com/certificates/fa5728523f994b2ea040e0d5cf8870f6",
+            "image_url": "https://github.com/SamanKashanchi/streamlit_test/blob/main/TestDomePython.PNG",
             "organization": "TestDome",
             "details_link": "https://www.testdome.com/certificates/fa5728523f994b2ea040e0d5cf8870f6"
         }
