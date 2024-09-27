ef8d041eb67e76f147febddbf7d5abb02f0e0465
SamanKashanchi <60948428+SamanKashanchi@users.noreply.github.com>
2024-09-27 10:52:27 -0700

    Update 5_Certifications.py

--- pages/5_Certifications.py
+++ pages/5_Certifications.py
@@ -14,32 +14,34 @@ def show_certifications():
             "organization": "TestDome",
             "details_link": "https://www.testdome.com/certificates/fa5728523f994b2ea040e0d5cf8870f6"
         },
-        ,
         {
-            "title": "AWS Certified Solutions Architect",
+            "title": "HackerRank Problem Solving Certificate",
             "date": "August 2023",
-            "image_file": "AWSCertification.PNG",  # Local file path
-            "organization": "Amazon Web Services",
-            "details_link": "https://aws.amazon.com/certification/certified-solutions-architect-associate/"
+            "iframe_link": "https://www.hackerrank.com/certificates/iframe/e26e93338e02",
+            "organization": "HackerRank"
         }
     ]
 
     # Show number of results
-    st.write(f"Showing {len(certifications)} certification:")
+    st.write(f"Showing {len(certifications)} certifications:")
 
     # Display certificate details
     for cert in certifications:
         c1, c2 = st.columns([1, 3.5])
-        with c1:
-            st.image(cert["image_file"], use_column_width=True, width=500)  # Adjust width to 300px
+        if "image_file" in cert:
+            with c1:
+                st.image(cert["image_file"], use_column_width=True, width=500)  # Adjust width to 500px
         with c2:
             st.markdown(f"""
                 ### {cert['title']}
                 - **Date:** {cert['date']}
                 - **Organization:** {cert['organization']}
             """)
-            st.markdown(f"[View Certificate]({cert['details_link']})")
-
+            if "details_link" in cert:
+                st.markdown(f"[View Certificate]({cert['details_link']})")
+            if "iframe_link" in cert:
+                # Embed the iframe
+                st.components.v1.iframe(cert["iframe_link"], height=500, scrolling=True)
 
 if __name__ == "__main__":
     show_certifications()
